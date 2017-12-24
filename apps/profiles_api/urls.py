f85acd052fd4914a3ec146997d03b43ad7c778a9
Surferito <emiliomellado@icloud.com>
2017-12-24 23:25:29 +0000

    anadimos en vistas viewset y router en url

--- apps/profiles_api/urls.py
+++ apps/profiles_api/urls.py
@@ -1,7 +1,11 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from . import views
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('viewset', views.HelloViewSet, base_name='hello-viewset')
 
 urlpatterns = [
     url(r'^cabify', views.Cabify.as_view(), name='cabify'),
-
+    url(r'', include(router.urls))
 ]
