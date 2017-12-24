b7d10f02ea2b7c5c0e9c868e100fa1de8845660c
Surferito <emiliomellado@icloud.com>
2017-12-24 14:20:04 +0000

    arreglamos para que el usuario se pueda registrar y logear con email y password solo

--- apps/profiles_api/models.py
+++ apps/profiles_api/models.py
@@ -5,24 +5,24 @@ from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseU
 class UserProfileManager(BaseUserManager):
     ''' Helps django work with out custom user model '''
 
-    def create_user(self, name, email, password=None):
+    def create_user(self, email, password=None):
         ''' creates a new user profile object '''
 
         if not email:  # if email== none or false
             raise ValueError('Users must have an email address')
 
         email = self.normalize_email(email)  # pone el email en minusculas entero
-        user = self.model(email=email, name=name)  # crea un objeto usuario
+        user = self.model(email=email,)  # crea un objeto usuario
 
         user.set_password(password)
         user.save()
 
         return user
 
-    def create_superuser(self, name, email, password):
+    def create_superuser(self, email, password):
         ''' creates a new superuser '''
 
-        user = self.create_user(email, name, password)  # crea un objeto usuario
+        user = self.create_user(email, password)  # crea un objeto usuario
 
         user.is_superuser = True
         user.is_staff = True
@@ -36,7 +36,7 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
     ''' Represents a user profile inside our system '''
 
     email = models.EmailField(max_length=255, unique=True)
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, blank=True)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
@@ -44,7 +44,7 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
 
     # esta variable permite al usuario logearse con su email
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['name']
+
 
     def get_full_name(self):
         ''' Use to get a users full name '''
