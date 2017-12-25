f0b59fa067f38dbbff66c9e770c3ddbba4bbb8b2
Surferito <emiliomellado@icloud.com>
2017-12-25 15:18:37 +0000

    metemos metodos de create, retrieve, update, partial update y destroy para la clase ViewSet  de cabify

--- apps/profiles_api/views.py
+++ apps/profiles_api/views.py
@@ -54,15 +54,46 @@ class Cabify(APIView):
         return Response({'method': 'delete'})
 
 class HelloViewSet(viewsets.ViewSet):
-    """ Test API ViewSet """
+    """Test API ViewSet."""
+
+    serializer_class = serializers.CabifySerializer
 
     def list(self, request):
-        """ Return a hello message. """
+        """Return a hello message."""
 
         a_viewset = [
-            'Uses actions (list, create, retrieve, update, partial_update)'
-            'Automatically maps to URLS using Routers',
-            'Provides more functionality with less code'
+            'Uses actions (list, create, retrieve, update, partial_update)',
+            'Automatically maps to URLs using Routers',
+            'Provides more functionality with less code.'
         ]
 
         return Response({'message': 'Hello!', 'a_viewset': a_viewset})
+
+    def create(self, request):
+        """Create a new hello message."""
+
+        serializer = serializers.CabifySerializer(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.data.get('name')
+            surname = serializer.data.get('surname')
+            message = 'Hello {0}'.format(name)
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, request, pk=None):
+        """Handles getting an object by its ID."""
+
+        return Response({'http_method': 'GET'})
+
+    def update(self, request, pk=None):
+        """Handles updating an object."""
+
+        return Response({'http_method': 'PUT'})
+
+    def partial_update(self, request, pk=None):
+        """Handles updating part of an object."""
+
+        return Response({'http_method': 'PATCH'})
