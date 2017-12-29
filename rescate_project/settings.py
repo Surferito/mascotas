77b0eecb42349a3233d1e37b248bc633673d424c
Surferito <emiliomellado@icloud.com>
2017-12-29 00:59:19 +0000

    probamos otra confi de static3

--- rescate_project/settings.py
+++ rescate_project/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 #Para configurar los archivos estaticos a nivel aplicaciones
-STATIC_URL = '/static/static'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'static')
 #STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
 
